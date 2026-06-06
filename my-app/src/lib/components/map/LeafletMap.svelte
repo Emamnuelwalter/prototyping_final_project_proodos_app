85711222d854c0ed300b1e19042b08b8d5c8aa68
Emamnuelwalter <walteremm@gmail.com>
2026-06-06 02:30:40 +0200

    leafleat icon fixer

--- my-app/src/lib/components/map/LeafletMap.svelte
+++ my-app/src/lib/components/map/LeafletMap.svelte
@@ -1,6 +1,9 @@
 <script>
   import { onMount } from "svelte";
   import "leaflet/dist/leaflet.css";
+  import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+  import markerIcon from "leaflet/dist/images/marker-icon.png";
+  import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
   let { offers = [], selectedOffer = null, onSelectOffer = null } = $props();
 
@@ -129,6 +132,14 @@
 
   onMount(async () => {
     L = await import("leaflet");
+
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: markerIcon2x,
+      iconUrl: markerIcon,
+      shadowUrl: markerShadow,
+    });
 
     map = L.map(mapElement).setView([47.3769, 8.5417], 12);
 
